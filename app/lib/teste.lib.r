a12172d5c918deaa00f1c2748a631c52cd3e2e88
Andrei Felipe <sousandrei@gmail.com>
2016-10-22 15:27:02 -0200

    rscript

---
+++ app/lib/teste.lib.r
@@ -0,0 +1,8 @@
+# RScript aqui
+# Executar com 'rscript teste.lib.r "{a: 0}"' que, chama o arquivo e passa uma string que e um json
+
+# Recebe json
+args = commandArgs(trailingOnly=TRUE)
+
+# printa um json de volta
+args[0]
