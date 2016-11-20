11c00ac3223251fb82c9af4fecffdd653f95d639
Henrique Pires Goulart dos Santos <hpgsantos@gmail.com>
2016-11-20 21:39:58 -0200

    filtered-tweet.lib. filtro words
    Versao inicial, filtro ainda nao parametrizavel via stdin

---
+++ app/lib/r/filtered-tweet.lib.r
@@ -0,0 +1,252 @@
+
+## Filtra twittes por palavras. Caso exista mais de uma separar por virgula
+## Apresenta como size no node a quantidade de retweets
+## Relacao dos edges e por tweet e word
+
+# Formato do json a ser retornado: 
+#{
+#	'nodes': [
+#		{
+#			'id': 'id do tweet',
+#			'label': 'tweet text',
+#     'size': 'qtd retweet'
+#		}
+#	],
+#	'edges': [
+#		{
+#			'from': 'id do tweet',
+#			'to': 'id do tweet',
+#     'label': 'word'
+#		}
+#	]
+#}
+
+if(!require(RCurl)) install.packages('RCurl',dependencies=TRUE, repos="http://cran.rstudio.com/")
+if(!require(jsonlite)) install.packages('jsonlite',dependencies=TRUE, repos="http://cran.rstudio.com/")
+if(!require(digest)) install.packages('digest',dependencies=TRUE, repos="http://cran.rstudio.com/")
+    
+
+library(RCurl)
+library(jsonlite)
+library(digest)
+
+
+concatWord <- function(bagwDF, minOccur=0) {
+    tryCatch({
+        
+        concatDF <- aggregate(bagwDF$id,bagwDF['word'],paste,collapse=',')
+        colnames(concatDF) <- c("word", "id")
+        #caso tenha mais de minOccur iteracoes da palavra
+        concatDF <- concatDF[countCharOccurrences(',', concatDF$id) > minOccur, ]
+        rownames(concatDF) <- NULL
+        return(concatDF)
+        
+    }, error=function(e){ 
+        #print(i) 
+        #print(e) 
+    })
+}
+
+countCharOccurrences <- function(char, s) {
+    s2 <- gsub(char,"",s)
+    return (nchar(s) - nchar(s2))
+}
+
+cleanText <- function(text, join = TRUE) {
+    pangram <- text
+    pangram <- gsub("^RT([^:]*.)","",pangram, perl=TRUE)
+    pangram <- gsub("[[:punct:]]", "", pangram)
+    
+    if(join) {
+      pangram <- gsub("\\s", "", pangram)  
+    }
+    
+    return (pangram)
+}
+
+shirnkText <- function(text) {
+    sntzText <- cleanText(text, FALSE)
+    sntzText <- substr(sntzText, 1, 139)
+    sntzText <- tolower(sntzText)
+    return (sntzText)
+}
+
+checksumText <- function(text) {
+    
+    sntzText <- cleanText(text)
+    sntzText <- substr(sntzText, 2, 30)
+    
+    cehcksum <- digest::digest(sntzText,algo='md5', serialize = FALSE)
+    return (cehcksum)
+}
+
+vectorFind <- function(patt, text) {
+    
+    cPatt <- unlist(strsplit(patt, ","))
+    cLen <- length(cPatt)
+ 
+    for(i in 1:cLen) {
+        print(grepl(cPatt[i], text))
+    }
+}
+
+permuteUser <- function(userArr, twoWay = TRUE, colName = c("from","to")) {
+    
+    lnkdUser <- NULL
+    
+    if(twoWay) {
+        lnkdUser <- expand.grid(userArr,userArr)
+        colnames(lnkdUser) <- colName
+        lnkdUser <- lnkdUser[which(lnkdUser$from != lnkdUser$to),]
+        row.names(lnkdUser) <- NULL           
+    } else {
+            
+        lnkdUser <- combn(userArr, 2, simplify=FALSE) 
+    }
+    
+    return (lnkdUser)
+}
+
+URL <- "localhost:1234/collection/Local/twitter/tweets/export/true"
+rawJson <- getURLContent(URL)
+rd <- fromJSON(rawJson)
+bagTwt <- data.frame(rd$id, rd$text,rd$user, rd$retweet_count, rd$retweeted_status)
+colnames(bagTwt) <- c("id","text","user","retweet_count","retweeted_status")
+##gera o md5 do texto do twiter
+bagTwt$chksum <- sapply(bagTwt$text, checksumText)
+bagTwt$text_small <- sapply(bagTwt$text, shirnkText)
+bagTwt[, 4] <- as.numeric(as.character( bagTwt[, 4] ))
+
+
+bagTwt <- bagTwt[which(is.na(bagTwt$retweeted_status)),]
+bagTwt <- aggregate(bagTwt$retweet_count, by=list(id=bagTwt$chksum, text=bagTwt$text_small), FUN=sum)
+colnames(bagTwt) <- c("id","text","qtd_rtw")
+
+
+#head(bagTwt)
+
+pattern <- "musical,murica,trump"
+pattern <- tolower(pattern)
+pattVect <- unlist(strsplit(pattern, ","))
+pattVectLen <- length(pattVect)
+
+
+bgwDF  <- data.frame(id= character(0), word= character(0), qtd = integer(0))
+
+
+for(i in 1:pattVectLen){
+    
+    pattLower <- pattVect[i]
+
+    filteredDF <- bagTwt[which(grepl(pattLower,bagTwt$text)),]
+    ftdLen <- nrow(filteredDF)
+    
+    if(ftdLen > 0) {
+        for (j in 1:ftdLen) {
+            bgwDF <- rbind( bgwDF, data.frame(filteredDF$id[j],pattLower,1 ))
+        }           
+    }
+
+    #filteredDF <- NULL
+
+}
+colnames(bgwDF) <- c("id","word","qtd")
+#bgwDF
+
+bgwDF <- aggregate( as.matrix(bgwDF[,3]), as.list(bgwDF[,1:2]), FUN = sum)
+bgwDF.concat <-concatWord(bgwDF,-1)
+len <- nrow(bgwDF.concat)
+#head(bgwDF.concat, 50)
+
+
+## CRIA ARQUIVO DE SAIDA
+
+logFile <- tempfile()
+
+cat("", file=logFile, append=FALSE, sep = "")
+
+ukTwt <- unique(unlist(unique(strsplit(bgwDF.concat$id, split = ","), use.names = FALSE)))
+ukTwt <- ukTwt[!is.na(ukTwt)]
+ukTwtLen <- length(ukTwt)
+
+ukIdDF  <- data.frame(id= character(0), label= character(0), size = integer(0))
+bagTwtLen <- nrow(bagTwt)
+
+if(bagTwtLen > 0) { 
+    for(i in 1:ukTwtLen){
+        for(j in 1:bagTwtLen) {
+            if(ukTwt[i] == bagTwt$id[j]) {
+                ukIdDF <- rbind( ukIdDF, data.frame(ukTwt[i],bagTwt$text[j],bagTwt$qtd_rtw[j] ))
+                break
+            }   
+        }
+    }    
+}
+colnames(ukIdDF) <- c("id","label","size")
+#head(ukIdDF)
+
+## CRIA OS NODES DO ARQUIVO JSON graphi
+
+nodeDF <- ukIdDF
+colnames(nodeDF) <- c("id","label","size")
+ukUsr <- NULL
+
+cat('{', file=logFile, append=TRUE, sep = "\n")
+cat('"nodes": [', file=logFile, append=TRUE, sep = "\n")
+
+##output como JS
+##cat('var nodes = [', file=logFile, append=TRUE, sep = "\n")
+
+
+nodeJson <- toJSON(nodeDF, pretty = FALSE)
+nodeJson <- gsub("^\\[", "", nodeJson)
+nodeJson <- gsub("\\]$", "", nodeJson)
+
+cat(nodeJson, file=logFile, append=TRUE, sep = "\n")
+
+cat(']', file=logFile, append=TRUE, sep = "\n")
+cat(',', file=logFile, append=TRUE, sep = "\n")
+
+##output como JS
+##cat('];', file=logFile, append=TRUE, sep = "\n")
+
+# cria os edges do visio JSON graphi
+#bgwDF.concat
+len <- nrow(bgwDF.concat)
+
+edgeStr <- NULL
+cat('"edges": [', file=logFile, append=TRUE, sep = "\n")
+
+##output como JS
+#cat('var edges = [', file=logFile, append=TRUE, sep = "\n")
+
+for (i in 1:len){
+    
+    usrWord <- as.character(bgwDF.concat[i,1])
+    usrArr <- strsplit(bgwDF.concat[i,2], ",")[[1]]
+    usrPermute <- (permuteUser(usrArr, TRUE))
+   
+  
+    usrPermute$label <- usrWord 
+     
+    edgeJson <- toJSON(usrPermute, pretty = FALSE)
+    edgeJson <- gsub("^\\[", "", edgeJson)
+    edgeJson <- gsub("\\]$", "", edgeJson)
+    
+    cat(edgeJson, file=logFile, append=TRUE, sep = "")
+    
+    if(i < len ) {
+        cat(",", file=logFile, append=TRUE, sep = "\n")
+    } 
+}
+
+cat('\n]', file=logFile, append=TRUE, sep = "\n")
+cat('}', file=logFile, append=TRUE, sep = "\n")
+
+##output como JS
+##cat('\n];', file=logFile, append=TRUE, sep = "\n")
+
+strOut <- readChar(logFile, file.info(logFile)$size)
+
+unlink(logFile, recursive=TRUE)
+write(strOut, stdout())
