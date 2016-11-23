a0375e6f6b231c74ace4fdecb280dc61cb81c4f6
Henrique Pires Goulart dos Santos <hpgsantos@gmail.com>
2016-11-23 20:47:46 -0200

    Size e output quando vazio

--- app/lib/r/filtered-tweet.lib.r
+++ app/lib/r/filtered-tweet.lib.r
@@ -2,7 +2,9 @@
 ## Apresenta como size no node a quantidade de retweets
 ## Relacao dos edges e por tweet e word
 
-## echo json_content | Rscript app/lib/filtered-tweet.lib.r --word=the --rtwt=200 --label
+##  Rscript app/lib/r/filtered-tweet.lib.r --word=business,orange --rtwt=-1
+
+
 # Formato do json a ser retornado: 
 #{
 #	'nodes': [
@@ -21,13 +23,16 @@
 #	]
 #}
 
+
 if(!require(RCurl,quietly = TRUE)) install.packages('RCurl',dependencies=TRUE, repos="http://cran.rstudio.com/")
 if(!require(jsonlite,quietly = TRUE)) install.packages('jsonlite',dependencies=TRUE, repos="http://cran.rstudio.com/")
 if(!require(digest,quietly = TRUE)) install.packages('digest',dependencies=TRUE, repos="http://cran.rstudio.com/")
-
+    
 suppressWarnings(suppressMessages(library(RCurl)))
 suppressWarnings(suppressMessages(library(jsonlite)))
 suppressWarnings(suppressMessages(library(digest)))
+
+fNumber <- function(x) is.numeric(x) & !is.na(x)
 
 ## CLI inicio
 args = commandArgs(TRUE)
@@ -37,23 +42,41 @@ argsDF <- as.data.frame(do.call("rbind", parseArgs(args)))
 argsL <- as.list(as.character(argsDF$V2))
 names(argsL) <- argsDF$V1
 
-qtdRtwt <- -1
-pattern <- ""
+
+
+devNull <- NULL
 showLabel <- FALSE
-	
+qtdRtwt <- -1 
+pattern <- ""
+optOutput <- "json"
+
+outputType <- c("json","js")
+
 if( !is.null(argsL$rtwt)) {
-	qtdRtwt <- as.numeric(argsL$rtwt)
+    tryCatch({
+		argsL$rtwt <- suppressWarnings(as.integer(argsL$rtwt))
+		devNull <- ifelse(!is.na(argsL$rtwt) & argsL$rtwt > -1, qtdRtwt <- argsL$rtwt, qtdRtwt <- -1)
+    }, error=function(e){ 
+       qtdRtwt <- -1 
+    })	
 }
 
+
 if( !is.null(argsL$word)) {
-	pattern <- argsL$word
-} else {
-	print("Passar pelo menos a palavra de pesquisada no socket --word=palavra")
+	devNull <- ifelse(nchar(argsL$word) > 0, pattern <- tolower(argsL$word) , pattern <- "" )
 }
 	
 if( !is.null(argsL$label)) {
 	showLabel <- TRUE
+} 
+
+if(!is.null(argsL$output)) {
+	devNull <- ifelse(nchar(argsL$output) > 0, optOutput <- tolower(argsL$output) , optOutput <- "json" )	
 }
+
+
+devNull <- ifelse(!optOutput %in% outputType,optOutput <- "json", optOutput <- optOutput)
+
 ## CLI fim    
 
 concatWord <- function(bagwDF, minOccur=0) {
@@ -90,7 +113,7 @@ cleanText <- function(text, join = TRUE) {
 shirnkText <- function(text) {
     sntzText <- cleanText(text, FALSE)
     sntzText <- substr(sntzText, 1, 139)
-    # sntzText <- tolower(sntzText)
+    sntzText <- tolower(sntzText)
     return (sntzText)
 }
 
@@ -119,21 +142,23 @@ permuteUser <- function(userArr, twoWay = TRUE, colName = c("from","to")) {
     
     if(twoWay) {
         lnkdUser <- expand.grid(userArr,userArr)
-        colnames(lnkdUser) <- colName
+		colnames(lnkdUser) <- colName
         lnkdUser <- lnkdUser[which(lnkdUser$from != lnkdUser$to),]
-        row.names(lnkdUser) <- NULL           
     } else {
             
-        lnkdUser <- combn(userArr, 2, simplify=FALSE) 
+        lnkdUser <- combn(userArr, 2, simplify=FALSE)
+        lnkdUser <- as.data.frame(matrix(unlist(lnkdUser), ncol = 2, byrow = TRUE))
     }
     
+	colnames(lnkdUser) <- colName
+    row.names(lnkdUser) <- NULL 
+ 
     return (lnkdUser)
 }
 
-#URL <- "localhost:1234/collection/Local/twitter/tweets/export/true"
-URL <- "https://localhost/api/tweets"
-rawJson <- getURL(URL, ssl.verifyhost = 0L, ssl.verifypeer = 0L)
-# rawJson = readLines(file('stdin', 'r'), n=1)    
+URL <- "localhost:1234/collection/Local/twitter/tweets/export/true"
+rawJson <- getURLContent(URL)
+#rawJson = readLines(file('stdin', 'r'), n=1)    
 rd <- fromJSON(rawJson)
 bagTwt <- data.frame(rd$id, rd$text,rd$user, rd$retweet_count, rd$retweeted_status)
 colnames(bagTwt) <- c("id","text","user","retweet_count","retweeted_status")
@@ -157,7 +182,7 @@ pattern <- tolower(pattern)
 pattVect <- unlist(strsplit(pattern, ","))
 pattVectLen <- length(pattVect)
 
-bgwDF <- data.frame(id= character(0), word= character(0), qtd = integer(0))
+bgwDF  <- data.frame(id= character(0), word= character(0), qtd = integer(0))
 
 for(i in 1:pattVectLen){
     
@@ -178,143 +203,158 @@ for(i in 1:pattVectLen){
 colnames(bgwDF) <- c("id","word","qtd")
 #bgwDF
 if(nrow(bgwDF) < 1) {
-	##opt <- options(show.error.messages=FALSE) 
-	##on.exit(options(opt)) 
-	stop('{"nodes":[], "edges":[]}')
-}
 	
-bgwDF <- aggregate( as.matrix(bgwDF[,3]), as.list(bgwDF[,1:2]), FUN = sum)
-bgwDF.concat <-concatWord(bgwDF,-1)
-len <- nrow(bgwDF.concat)
-#head(bgwDF.concat, 50)
-
-
-## CRIA ARQUIVO DE SAIDA
-
-logFile <- tempfile()
-
-cat("", file=logFile, append=FALSE, sep = "")
-
-ukTwt <- unique(unlist(unique(strsplit(bgwDF.concat$id, split = ","), use.names = FALSE)))
-ukTwt <- ukTwt[!is.na(ukTwt)]
-ukTwtLen <- length(ukTwt)
-
-ukIdDF  <- data.frame(id= character(0), label= character(0), size = integer(0))
-bagTwtLen <- nrow(bagTwt)
-
-if(bagTwtLen > 0) { 
-    for(i in 1:ukTwtLen){
-        for(j in 1:bagTwtLen) {
-            if(ukTwt[i] == bagTwt$id[j]) {
-                ukIdDF <- rbind( ukIdDF, data.frame(ukTwt[i],bagTwt$text[j],bagTwt$qtd_rtw[j] ))
-                break
-            }   
-        }
-    }    
-}
-colnames(ukIdDF) <- c("id","label","size")
-#head(ukIdDF)
-
-## CLUSTER INICIO
-ukIdDFScale <- na.omit(ukIdDF$size)
-ukIdDFScale <- scale(ukIdDFScale)
-qtdUkIdDFScale <- nrow(unique(ukIdDFScale))
-qtdCluster <- 0
-
-wss <- (nrow(ukIdDFScale)-1)*sum(apply(ukIdDFScale,2,var))
-
-if(!is.na(wss)){
-	qtdCluster <- as.numeric(wss[1])
-}
-##normalizacao clusters e escalas unicas
-if( qtdCluster > qtdUkIdDFScale) {
-	qtdCluster <- qtdUkIdDFScale
-}	
-
-if(qtdCluster > 10) {
-	qtdCluster <- 10
-	qtdUkIdDFScale <- 10
-}
-if(qtdCluster > 2) {
-	fit <- kmeans(ukIdDFScale, qtdCluster)
-	ukIdDFScale <- data.frame(ukIdDFScale, fit$cluster)
-	colnames(ukIdDFScale) <- c( "scale", "cluster" )  
-	ukIdDF <- cbind(ukIdDF,ukIdDFScale)
-}
-## CLUSTER FIM
-
-## CRIA OS NODES DO ARQUIVO JSON graphi
-
-if(qtdCluster > 2) {
-	nodeDF <- ukIdDF[,c("id","label","size","cluster")]
-	nodeDF <- within(nodeDF,  cluster <- paste("G",as.character(cluster), sep=""))
+	opt <- options(show.error.messages=FALSE) 
+	on.exit(options(opt)) 
+	write('{"nodes":[], "edges":[]}', stdout())
+	
 } else {
-	nodeDF <- ukIdDF[,c("id","label","size")]
-	nodeDF <- within(nodeDF,  cluster <- paste("G1", sep=""))
+	
+	bgwDF <- aggregate( as.matrix(bgwDF[,3]), as.list(bgwDF[,1:2]), FUN = sum)
+	bgwDF.concat <-concatWord(bgwDF,-1)
+	len <- nrow(bgwDF.concat)
+	#head(bgwDF.concat, 50)
+
+
+	## CRIA ARQUIVO DE SAIDA
+
+	logFile <- tempfile()
+
+	cat("", file=logFile, append=FALSE, sep = "")
+
+	ukTwt <- unique(unlist(unique(strsplit(bgwDF.concat$id, split = ","), use.names = FALSE)))
+	ukTwt <- ukTwt[!is.na(ukTwt)]
+	ukTwtLen <- length(ukTwt)
+
+	ukIdDF  <- data.frame(id= character(0), label= character(0), size = integer(0))
+	bagTwtLen <- nrow(bagTwt)
+
+	if(bagTwtLen > 0) { 
+		for(i in 1:ukTwtLen){
+			for(j in 1:bagTwtLen) {
+				if(ukTwt[i] == bagTwt$id[j]) {
+					size <- as.numeric(bagTwt$qtd_rtw[j])
+				
+					devNull <- ifelse(!is.na(size) & size > 0,size <- size,size <-1 )
+					
+					if(size > 10) {
+						size <- 10
+					}
+					
+					ukIdDF <- rbind( ukIdDF, data.frame(ukTwt[i],bagTwt$text[j],bagTwt$qtd_rtw[j], size ))
+					break
+				}   
+			}
+		}    
+	}
+	colnames(ukIdDF) <- c("id","label","rtwt","size")
+	#head(ukIdDF)
+
+	## CLUSTER INICIO
+	ukIdDFScale <- na.omit(ukIdDF$rtwt)
+	ukIdDFScale <- scale(ukIdDFScale)
+	qtdUkIdDFScale <- nrow(unique(ukIdDFScale))
+	qtdCluster <- 0
+
+	wss <- (nrow(ukIdDFScale)-1)*sum(apply(ukIdDFScale,2,var))
+
+	if(!is.na(wss)){
+		qtdCluster <- as.numeric(wss[1])
+	}
+	##normalizacao clusters e escalas unicas
+	if( qtdCluster > qtdUkIdDFScale) {
+		qtdCluster <- qtdUkIdDFScale
+	}	
+
+	if(qtdCluster > 10) {
+		qtdCluster <- 10
+		qtdUkIdDFScale <- 10
+	}
+	if(qtdCluster > 2) {
+		fit <- kmeans(ukIdDFScale, qtdCluster)
+		ukIdDFScale <- data.frame(ukIdDFScale, fit$cluster)
+		colnames(ukIdDFScale) <- c( "scale", "cluster" )  
+		ukIdDF <- cbind(ukIdDF,ukIdDFScale)
+	}
+	## CLUSTER FIM
+
+	## CRIA OS NODES DO ARQUIVO JSON graphi
+
+	if(qtdCluster > 2) {
+		nodeDF <- ukIdDF[,c("id","label","size","cluster")]
+		nodeDF <- within(nodeDF,  cluster <- paste("G",as.character(cluster), sep=""))
+	} else {
+		nodeDF <- ukIdDF[,c("id","label","size")]
+		nodeDF <- within(nodeDF,  cluster <- paste("G1", sep=""))
+	}
+
+	colnames(nodeDF) <- c("id","label","size","group")
+	ukUsr <- NULL
+
+	if (optOutput == 'json') {
+		cat('{', file=logFile, append=TRUE, sep = "\n")
+		cat('"nodes": [', file=logFile, append=TRUE, sep = "\n")
+	} else if(optOutput == 'js') {
+		cat('var nodes = [', file=logFile, append=TRUE, sep = "\n")
+	}
+
+	nodeJson <- toJSON(nodeDF, pretty = FALSE)
+	nodeJson <- gsub("^\\[", "", nodeJson)
+	nodeJson <- gsub("\\]$", "", nodeJson)
+
+	cat(nodeJson, file=logFile, append=TRUE, sep = "\n")
+	
+	if (optOutput == 'json') {
+		cat(']', file=logFile, append=TRUE, sep = "\n")
+		cat(',', file=logFile, append=TRUE, sep = "\n")
+	} else if(optOutput == 'js' ) {
+		cat('];', file=logFile, append=TRUE, sep = "\n")
+	}
+
+	# cria os edges do visio JSON graphi
+	#bgwDF.concat
+	len <- nrow(bgwDF.concat)
+
+	edgeStr <- NULL
+	if (optOutput == 'json') {
+		cat('"edges": [', file=logFile, append=TRUE, sep = "\n")
+	} else if(optOutput == 'js') {
+		cat('var edges = [', file=logFile, append=TRUE, sep = "\n")
+	}
+
+	for (i in 1:len){
+		
+		usrWord <- as.character(bgwDF.concat[i,1])
+		usrArr <- strsplit(bgwDF.concat[i,2], ",")[[1]]
+		p <- i + 1
+		
+		if(grepl(",", bgwDF.concat[i,2])) {
+			usrPermute <- (permuteUser(usrArr, FALSE))
+
+			if(showLabel) {
+				usrPermute$label <- usrWord 
+			}
+			edgeJson <- toJSON(usrPermute, pretty = FALSE)
+			edgeJson <- gsub("^\\[", "", edgeJson)
+			edgeJson <- gsub("\\]$", "", edgeJson)
+		
+			cat(edgeJson, file=logFile, append=TRUE, sep = "")
+		
+			if(i < len & grepl(",", bgwDF.concat[p,2]) ) {
+				cat(",", file=logFile, append=TRUE, sep = "\n")
+			}
+		}
+	}
+	
+	if (optOutput == 'json') {
+		cat('\n]', file=logFile, append=TRUE, sep = "\n")
+		cat('}', file=logFile, append=TRUE, sep = "\n")
+	} else if(optOutput == 'js') {
+		cat('\n];', file=logFile, append=TRUE, sep = "\n")
+	}
+
+	strOut <- readChar(logFile, file.info(logFile)$size)
+
+	unlink(logFile, recursive=TRUE)
+	write(strOut, stdout())
 }
-
-colnames(nodeDF) <- c("id","label","size","group")
-ukUsr <- NULL
-
-cat('{', file=logFile, append=TRUE, sep = "\n")
-cat('"nodes": [', file=logFile, append=TRUE, sep = "\n")
-
-##output como JS
-##cat('var nodes = [', file=logFile, append=TRUE, sep = "\n")
-
-nodeJson <- toJSON(nodeDF, pretty = FALSE)
-nodeJson <- gsub("^\\[", "", nodeJson)
-nodeJson <- gsub("\\]$", "", nodeJson)
-
-cat(nodeJson, file=logFile, append=TRUE, sep = "\n")
-
-cat(']', file=logFile, append=TRUE, sep = "\n")
-cat(',', file=logFile, append=TRUE, sep = "\n")
-
-##output como JS
-##cat('];', file=logFile, append=TRUE, sep = "\n")
-
-# cria os edges do visio JSON graphi
-#bgwDF.concat
-len <- nrow(bgwDF.concat)
-
-edgeStr <- NULL
-cat('"edges": [', file=logFile, append=TRUE, sep = "\n")
-
-##output como JS
-#cat('var edges = [', file=logFile, append=TRUE, sep = "\n")
-
-for (i in 1:len){
-    
-    usrWord <- as.character(bgwDF.concat[i,1])
-    usrArr <- strsplit(bgwDF.concat[i,2], ",")[[1]]
-    p <- i + 1
-    
-    if(grepl(",", bgwDF.concat[i,2])) {
-	usrPermute <- (permuteUser(usrArr, TRUE))
-
-    	if(showLabel) {
-    		usrPermute$label <- usrWord 
-    	}
-    	edgeJson <- toJSON(usrPermute, pretty = FALSE)
-    	edgeJson <- gsub("^\\[", "", edgeJson)
-    	edgeJson <- gsub("\\]$", "", edgeJson)
-    
-    	cat(edgeJson, file=logFile, append=TRUE, sep = "")
-    
-    	if(i < len & grepl(",", bgwDF.concat[p,2]) ) {
-        	cat(",", file=logFile, append=TRUE, sep = "\n")
-    	}
-    }
-}
-
-cat('\n]', file=logFile, append=TRUE, sep = "\n")
-cat('}', file=logFile, append=TRUE, sep = "\n")
-
-##output como JS
-##cat('\n];', file=logFile, append=TRUE, sep = "\n")
-
-strOut <- readChar(logFile, file.info(logFile)$size)
-
-unlink(logFile, recursive=TRUE)
-write(strOut, stdout())
